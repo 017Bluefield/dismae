a4dea0eeb031cad20324240b2cecefbab4302935
Jeremy Miller <jeremy@dischan.org>
2016-05-01 14:27:21 -0400

    Fade in button too.

--- src/dismae/states/MainMenu.js
+++ src/dismae/states/MainMenu.js
@@ -15,6 +15,9 @@ window.Dismae.MainMenu.prototype = {
     this.add.tween(title).to({alpha: 1}, 2000, window.Phaser.Easing.Linear.None, true, 0)
 
     this.playButton = this.add.button(this.ratio * 400, this.ratio * 600, 'playButton', this.startGame, this, 'over', 'out', 'down')
+    this.playButton.alpha = 0
+    this.playButton.scale.setTo(this.ratio)
+    this.add.tween(this.playButton).to({alpha: 1}, 2000, window.Phaser.Easing.Linear.None, true, 0)
   },
 
   update: function () {},
